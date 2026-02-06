83e94a9556c1019fd36c1d870dd2f686e97e081c
Aekkaphop013 <aekkaphop.aunma@g.swu.ac.th>
2026-02-06 16:34:27 +0700

    Add files via upload

---
+++ CraftDesign/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassDesign;
+
+namespace CraftDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new ClassDesignTest().Run();
+        }
+    }
+}
